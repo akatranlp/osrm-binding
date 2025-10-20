3c98f0f5957c8e3738cdeafed2fea07983459845
Fabian Petersen <fabian@nf-petersen.de>
2025-10-20 19:17:50 +0200

    add additional fields to response type

--- src/route.rs
+++ src/route.rs
@@ -1,10 +1,11 @@
-use derive_builder::Builder;
 use crate::point::Point;
-use serde::{Deserialize, Serialize};
 use crate::waypoints::Waypoint;
-#[derive(Debug, Builder, Clone)]
+use derive_builder::Builder;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Builder)]
 pub struct RouteRequest {
-    pub points : Vec<Point>,
+    pub points: Vec<Point>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -34,9 +35,10 @@ pub struct OsrmResponse {
 pub struct Route {
     pub legs: Vec<Leg>,
     pub weight_name: String,
-    pub geometry: String,
+    pub geometry: Geometry,
     pub weight: f64,
     pub duration: f64,
+    pub distance: f64,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -48,6 +50,121 @@ pub struct Leg {
     pub distance: f64,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Geometry {
+    pub coordinates: Vec<[f64; 2]>,
+    #[serde(rename = "type")]
+    pub geometry_type: GeometryType,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum GeometryType {
+    #[serde(rename = "LineString")]
+    LineString,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Step {
+    pub intersections: Vec<Intersection>,
+    pub maneuver: Maneuver,
+    pub name: String,
+    pub duration: f64,
+    pub distance: f64,
+    pub driving_side: DrivingSide,
+    pub weight: f64,
+    pub mode: Mode,
+    pub geometry: Geometry,
+    #[serde(rename = "ref")]
+    pub step_ref: Option<String>, // existed in both (already optional)
+    pub destinations: Option<String>, // only in second → optional
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum DrivingSide {
+    Left,
+    Right,
+    Straight,
+    // only present in some version(s) / strings:
+    #[serde(rename = "slight left")]
+    SlightLeft,
+    #[serde(rename = "slight right")]
+    SlightRight,
+    // only in second → include to be a superset:
+    None,
+    Uturn,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Intersection {
+    pub entry: Vec<bool>,
+    pub bearings: Vec<i64>,
+    pub location: [f64; 2],
+    #[serde(rename = "in")]
+    pub intersection_in: Option<i64>,
+
+    // present in both but optional in at least one:
+    pub out: Option<i64>,
+    pub lanes: Option<Vec<Lane>>,
+    pub classes: Option<Vec<String>>,
+
+    // only in first → make optional:
+    pub duration: Option<f64>,
+    pub admin_index: Option<i64>,
+    pub weight: Option<f64>,
+    pub geometry_index: Option<i64>,
+    pub turn_weight: Option<f64>,
+    pub turn_duration: Option<f64>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Lane {
+    pub indications: Vec<DrivingSide>,
+    pub valid: bool,
+
+    // only in first → optional:
+    pub active: Option<bool>,
+    pub valid_indication: Option<DrivingSide>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Maneuver {
+    pub bearing_after: i64,
+    pub bearing_before: i64,
+    pub location: [f64; 2],
+    pub modifier: Option<DrivingSide>,
+    #[serde(rename = "type")]
+    pub maneuver_type: ManeuverType,
+
+    // only in second → optional:
+    pub exit: Option<i64>,
+    // only in first → optional:
+    pub instruction: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ManeuverType {
+    Arrive,
+    Continue,
+    Depart,
+    #[serde(rename = "end of road")]
+    EndOfRoad,
+    Fork,
+    Turn,
+
+    // only in second → include to be a superset:
+    Merge,
+    #[serde(rename = "new name")]
+    NewName,
+    #[serde(rename = "on ramp")]
+    OnRamp,
+    #[serde(rename = "roundabout turn")]
+    RoundaboutTurn,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Mode {
+    Driving,
 }
