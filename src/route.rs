4fbba03c4ba8b199390d0d2bc541848458c47f5e
Fabian Petersen <fabian@nf-petersen.de>
2025-12-03 14:20:59 +0100

    fix maneuverType

--- src/route.rs
+++ src/route.rs
@@ -147,22 +147,45 @@ pub struct Maneuver {
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum ManeuverType {
-    Arrive,
-    Continue,
-    Depart,
-    #[serde(rename = "end of road")]
-    EndOfRoad,
-    Fork,
+    /// a basic turn into direction of the modifier
     Turn,
-
-    // only in second → include to be a superset:
-    Merge,
+    /// no turn is taken/possible, but the road name changes. The road can take a turn itself, following modifier .
     #[serde(rename = "new name")]
     NewName,
+    /// indicates the departure of the leg
+    Depart,
+    /// indicates the destination of the leg
+    Arrive,
+    /// merge onto a street (e.g. getting on the highway from a ramp, the modifier specifies the direction of the merge )
+    Merge,
+    /// take a ramp to enter a highway (direction given my modifier )
     #[serde(rename = "on ramp")]
     OnRamp,
+    /// take a ramp to exit a highway (direction given my modifier )
+    #[serde(rename = "off ramp")]
+    OffRamp,
+    /// take the left/right side at a fork depending on modifier
+    Fork,
+    /// road ends in a T intersection turn in direction of modifier
+    #[serde(rename = "end of road")]
+    EndOfRoad,
+    /// Turn in direction of modifier to stay on the same road
+    Continue,
+    /// traverse roundabout, if the route leaves the roundabout there will be an additional property exit for exit counting. The modifier specifies the direction of entering the roundabout.
+    Roundabout,
+    /// a traffic circle. While very similar to a larger version of a roundabout, it does not necessarily follow roundabout rules for right of way. It can offer rotary_name and/or rotary_pronunciation parameters (located in the RouteStep object) in addition to the exit parameter (located on the StepManeuver object).
+    Rotary,
+    /// Describes a turn at a small roundabout that should be treated as normal turn. The modifier indicates the turn direciton. Example instruction: At the roundabout turn left .
     #[serde(rename = "roundabout turn")]
     RoundaboutTurn,
+    /// not an actual turn but a change in the driving conditions. For example the travel mode or classes. If the road takes a turn itself, the modifier describes the direction
+    Notification,
+    /// Describes a maneuver exiting a roundabout (usually preceeded by a roundabout instruction)
+    #[serde(rename = "exit roundabout")]
+    ExitRoundabout,
+    /// Describes the maneuver exiting a rotary (large named roundabout)
+    #[serde(rename = "exit rotary")]
+    ExitRotary,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
